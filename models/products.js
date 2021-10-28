1974b64a530df410381ce449bf8284de16cd091d
fredd <fredd.oropeza@gmail.com>
2021-10-28 16:59:27 -0400

    Updates schema model

--- models/products.js
+++ models/products.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const snowSchema = new Schema({
+const productSchema = new Schema({
     title: String,
     img: String,
     description: String,
+    type: String, 
 }, { timestamps: true });
 
-module.exports = mongoose.model('Snow', snowSchema);
+module.exports = mongoose.model('Product', productSchema);
